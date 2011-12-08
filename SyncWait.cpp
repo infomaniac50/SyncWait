c6dbd624497c5d64d53b26fd0729810332141139
Derek Chafin <infomaniac50@gmail.com>
2011-12-08 11:28:12 -0600

    First Commit

---
+++ SyncWait.cpp
@@ -0,0 +1,45 @@
+/*
+  SyncWait.cpp - Library to synchronize the start of serial output with a program on a computer.
+  Created by Derek Chafin, December 6, 2011
+  Released into the public domain.
+*/
+
+#include "Arduino.h"
+#include "SyncWait.h"
+
+void SyncWait::doSyncWait(const byte* startBytes, int length)
+{
+  while(checkBytes(startBytes, length))
+  {
+    delay(50);
+  }
+}
+
+boolean SyncWait::doFinish(const byte* endBytes, int length)
+{
+  return checkBytes(endBytes, length);
+}
+
+boolean SyncWait::checkBytes(const byte* bytes, int length)
+{
+  boolean keepgoing = true;
+
+  if (Serial.available() >= length)
+  {      
+    for (int i=0; i < length; i++)
+    {
+      int data = Serial.read();
+      if (data == bytes[i])
+      {
+        keepgoing = false;
+      }
+      else
+      {
+        keepgoing = true;
+        break;
+      }
+    }
+  }
+  
+  return keepgoing;
+}
---
+++ SyncWait.h
@@ -0,0 +1,20 @@
+/*
+  SyncWait.h - Library to synchronize the start of serial output with a program on a computer.
+  Created by Derek Chafin, December 6, 2011
+  Released into the public domain.
+*/
+
+#ifndef SyncWait_h
+#define SyncWait_h
+
+#include "Arduino.h"
+
+class SyncWait
+{
+  public:
+    void doSyncWait(const byte* startBytes, int length);
+    boolean doFinish(const byte* endBytes, int length);
+  private:
+    boolean checkBytes(const byte* bytes, int length);
+};
+#endif
